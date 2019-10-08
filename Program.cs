e4b89682c0070ac753577550a4a9eb5b850e3c8c
QedimBabayev <49645572+QedimBabayev@users.noreply.github.com>
2019-10-08 19:45:57 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversearrayayElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //way1
+            //int[] array = { 1, 2, 3, 4, 5 };
+
+            //int[] reverseArray = new int[array.Length];
+
+            //for (int i = 0; i <= array.Length - 1; i++)
+            //{
+            //    reverseArray[i] = array[array.Length - i - 1];
+
+            //    Console.WriteLine(reverseArray[i]);
+            //}
+
+            //way2
+            //int[] array = { 1, 2, 3, 4, 5 };
+            //for (int i=array.Length-1; i>= 0; i--)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+
+
+        }
+
+    }
+    }
+
